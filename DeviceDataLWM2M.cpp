f144771de6fe764865038208450b0ffda0bda08e
Manuel Schappacher <manuel.schappacher@hs-offenburg.de>
2017-01-31 17:32:06 +0100

    added LWM2M Device Data implementation

---
+++ DeviceDataLWM2M.cpp
@@ -0,0 +1,146 @@
+/*
+ * --- License -------------------------------------------------------------- *
+ */
+
+/*
+ * Copyright (c) 2015
+ *
+ * Hochschule Offenburg, University of Applied Sciences
+ * Institute for reliable Embedded Systems
+ * and Communications Electronic (ivESK)
+ *
+ * All rights reserved
+*/
+
+/*
+ * --- Module Description --------------------------------------------------- *
+ */
+
+/**
+ * \file    DeviceDataLWM2M.h
+ * \author  Institute of reliable Embedded Systems
+ *          and Communication Electronics
+ * \date    $Date$
+ * \version $Version$
+ *
+ * \brief   Description of the LWM2M sensor data element.
+ *
+ *          The LWM2M Sensor data element describes a specific sensor data
+ *          that is accessible using the LWM2M protocol. Therefore it inherits
+ *          from the general sensor data interface and includes some additional
+ *          functions e.g. to access the according LMWM2M device.
+ */
+
+/*
+ * --- Includes ------------------------------------------------------------- *
+ */
+#include <DeviceDataLWM2M.h>
+#include <iostream>
+
+
+
+/*
+ * --- Methods Definition ----------------------------------------------------- *
+ */
+
+/*---------------------------------------------------------------------------*/
+/*
+* getValNative()
+*/
+int16_t DeviceDataLWM2M::getValNative( DeviceDataValue* val )
+{
+    int16_t ret = -1;
+
+    if( (mp_lwm2mSrv != NULL) &&
+        (mp_lwm2mSrv->hasDevice( mp_lwm2mRes->getParent()->getParent()->getName() )))
+    {
+        std::string str;
+
+        /* The Device with the according resource is available. So we can read
+         * the value from the device. */
+        ret = mp_lwm2mSrv->read( mp_lwm2mRes, str, NULL );
+
+        if( ret == 0 )
+        {
+            /* read was successful. Now we can transfer the data to the according
+             * value format. */
+            /* Data was read successfully. now put it to the
+             * according value buffer */
+            switch( val->getType() )
+            {
+                case DeviceDataValue::TYPE_INTEGER:
+                {
+                    int32_t ibuf;
+                    sscanf( str.c_str(), "%d", &ibuf );
+                    ret = val->setVal( ibuf );
+                    break;
+                }
+
+                case DeviceDataValue::TYPE_FLOAT:
+                {
+                    float fbuf;
+                    sscanf( str.c_str(), "%f", &fbuf );
+                    ret = val->setVal( fbuf );
+                    break;
+                }
+
+                case DeviceDataValue::TYPE_STRING:
+                    ret = val->setVal( str.c_str() );
+                    break;
+
+                default:
+                    ret = -1;
+                    break;
+            }
+        }
+    }
+    return ret;
+}
+
+/*---------------------------------------------------------------------------*/
+/*
+* setValNative()
+*/
+int16_t DeviceDataLWM2M::setValNative( const DeviceDataValue* val )
+{
+    int16_t ret = -1;
+    char buf[100];
+
+    if( (mp_lwm2mSrv != NULL) &&
+        (mp_lwm2mSrv->hasDevice( mp_lwm2mRes->getParent()->getParent()->getName() )))
+    {
+        /* Write the new data */
+        switch( val->getType() )
+        {
+            case DeviceDataValue::TYPE_INTEGER:
+                snprintf( buf, 100, "%d", val->getVal().i32 );
+                ret = 0;
+                break;
+
+            case DeviceDataValue::TYPE_FLOAT:
+                snprintf( buf, 100, "%f", val->getVal().f );
+                ret = 0;
+                break;
+
+            case DeviceDataValue::TYPE_STRING:
+                snprintf( buf, 100, "%s", val->getVal().cStr );
+                ret = 0;
+                break;
+
+            default:
+                ret = -1;
+                break;
+        }
+
+        if( ret == 0 )
+        {
+            /* write the data via LWM2M */
+            ret = mp_lwm2mSrv->write( mp_lwm2mRes, buf, NULL );
+        }
+    }
+    return ret;
+}
+
+
+
+
