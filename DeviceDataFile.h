2db1e4944b744ab3c4019bb654e4804610270841
Manuel Schappacher <manuel.schappacher@hs-offenburg.de>
2017-11-29 13:24:02 +0100

    added apache license header

--- DeviceDataFile.h
+++ DeviceDataFile.h
@@ -3,13 +3,27 @@
  */
 
 /*
- * Copyright (c) 2015
+ * Copyright 2017 NIKI 4.0 project team
  *
- * Hochschule Offenburg, University of Applied Sciences
- * Institute for reliable Embedded Systems
- * and Communications Electronic (ivESK)
+ * NIKI 4.0 was financed by the Baden-Württemberg Stiftung gGmbH (www.bwstiftung.de).
+ * Project partners are FZI Forschungszentrum Informatik am Karlsruher
+ * Institut für Technologie (www.fzi.de), Hahn-Schickard-Gesellschaft
+ * für angewandte Forschung e.V. (www.hahn-schickard.de) and
+ * Hochschule Offenburg (www.hs-offenburg.de).
+ * This file was developed by the Institute of reliable Embedded Systems
+ * and Communication Electronics
  *
- * All rights reserved
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
 */
 
 /*
